c30c7d802da99ca89facfbd852ab903ba181c45f
Ridham <ridamsharma33@gmail.com>
2021-06-23 18:38:51 +0530

    update

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,36 +1,30 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
-function InfoBox({ title, isRed, isGrey, active, cases, total, ...props }) {
-	return (
-		<Card
-			onClick={props.onClick}
-			className={`infoBox ${active && "infoBox--selected"} ${
-				isRed && "infoBox--red"
-			} ${isGrey && "infoBox--grey"}`}
-		>
-			<CardContent>
-				{/* Title */}
-				<Typography className="infoBox__title" color="textSecondary">
-					{title}
-				</Typography>
 
-				{/* Number of Cases */}
-				<h2
-					className={`infoBox__cases ${!isRed && "infoBox__cases--green"} ${
-						isGrey && "infoBox__cases--grey"
-					}`}
-				>
-					{props.isloading ? <i className="fa fa-cog fa-spin fa-fw" /> : cases}
-				</h2>
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
 
-				{/* Total Cases */}
-				<Typography className="infoBox__total" color="textSecondary">
-					{total} Total
-				</Typography>
-			</CardContent>
-		</Card>
-	);
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
--- src/util.js
+++ src/util.js
@@ -5,50 +5,48 @@ import { Circle, Popup } from "react-leaflet";
 const casesTypeColors = {
 	cases: {
 		hex: "#CC1034",
-		// rgb: "rgb(204,16,52)",
-		// half_op: "rgba(204,16,52,0.5)",
-		mulitiplier: 800,
+		rgb: "rgb(204, 16, 52)",
+		half_op: "rgba(204, 16, 52, 0.5)",
+		multiplier: 200,
 	},
-
 	recovered: {
-		hex: "#7DD71D",
-		// rgb: "rgb(125,215,29)",
-		// half_op: "rgba(125,215,29,0.5)",
-		mulitiplier: 1200,
+		hex: "#7dd71d",
+		rgb: "rgb(125, 215, 29)",
+		half_op: "rgba(125, 215, 29, 0.5)",
+		multiplier: 300,
 	},
-
 	deaths: {
-		hex: "#C0C0C0",
-		// rgb: "rgb(251,68,67)",
-		// half_op: "rgba(251,68,67,0.5)",
-		mulitiplier: 2000,
+		hex: "#fb4443",
+		rgb: "rgb(251, 68, 67)",
+		half_op: "rgba(251, 68, 67, 0.5)",
+		multiplier: 500,
 	},
 };
 
 export const sortData = (data) => {
-	const sortedData = [...data];
-
-	sortedData.sort((a, b) => b.cases - a.cases);
-
+	let sortedData = [...data];
+	sortedData.sort((a, b) => {
+		if (a.cases > b.cases) {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
 	return sortedData;
 };
 
 export const prettyPrintStat = (stat) =>
 	stat ? `+${numeral(stat).format("0.0a")}` : "+0";
 
-//Draw circles on the map
-export const showDataOnMap = (data, casesType) =>
+export const showDataOnMap = (data, casesType = "cases") =>
 	data.map((country) => (
 		<Circle
 			center={[country.countryInfo.lat, country.countryInfo.long]}
+			color={casesTypeColors[casesType].hex}
+			fillColor={casesTypeColors[casesType].hex}
 			fillOpacity={0.4}
-			pathOptions={{
-				color: casesTypeColors[casesType].hex,
-				fillColor: casesTypeColors[casesType].hex,
-			}}
 			radius={
-				Math.sqrt(country[casesType] / 10) *
-				casesTypeColors[casesType].mulitiplier
+				Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
 			}
 		>
 			<Popup>
@@ -56,7 +54,7 @@ export const showDataOnMap = (data, casesType) =>
 					<div
 						className="info-flag"
 						style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
-					/>
+					></div>
 					<div className="info-name">{country.country}</div>
 					<div className="info-confirmed">
 						Cases: {numeral(country.cases).format("0,0")}
